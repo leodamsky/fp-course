3f1c1837e3e697694689d6f669dfa1b8bfed0d45
leodamsky <vadim.leodamsky@gmail.com>
2020-04-05 22:59:00 +0300

    Monad

--- src/Course/Monad.hs
+++ src/Course/Monad.hs
@@ -37,7 +37,7 @@ instance Monad ExactlyOne where
     -> ExactlyOne a
     -> ExactlyOne b
   (=<<) =
-    error "todo: Course.Monad (=<<)#instance ExactlyOne"
+    bindExactlyOne
 
 -- | Binds a function on a List.
 --
@@ -49,7 +49,7 @@ instance Monad List where
     -> List a
     -> List b
   (=<<) =
-    error "todo: Course.Monad (=<<)#instance List"
+    flatMap
 
 -- | Binds a function on an Optional.
 --
@@ -61,7 +61,7 @@ instance Monad Optional where
     -> Optional a
     -> Optional b
   (=<<) =
-    error "todo: Course.Monad (=<<)#instance Optional"
+    bindOptional
 
 -- | Binds a function on the reader ((->) t).
 --
@@ -69,11 +69,11 @@ instance Monad Optional where
 -- 119
 instance Monad ((->) t) where
   (=<<) ::
-    (a -> ((->) t b))
+    (a -> (t -> b))
     -> ((->) t a)
     -> ((->) t b)
-  (=<<) =
-    error "todo: Course.Monad (=<<)#instance ((->) t)"
+  (=<<) fatb ta t =
+    fatb (ta t) t
 
 -- | Witness that all things with (=<<) and (<$>) also have (<*>).
 --
@@ -111,8 +111,8 @@ instance Monad ((->) t) where
   k (a -> b)
   -> k a
   -> k b
-(<**>) =
-  error "todo: Course.Monad#(<**>)"
+(<**>) fab fa =
+  (<$> fa) =<< fab
 
 infixl 4 <**>
 
@@ -133,8 +133,8 @@ join ::
   Monad k =>
   k (k a)
   -> k a
-join =
-  error "todo: Course.Monad#join"
+join ffa =
+  id =<< ffa
 
 -- | Implement a flipped version of @(=<<)@, however, use only
 -- @join@ and @(<$>)@.
@@ -147,8 +147,8 @@ join =
   k a
   -> (a -> k b)
   -> k b
-(>>=) =
-  error "todo: Course.Monad#(>>=)"
+(>>=) ka akb =
+ join $ akb <$> ka
 
 infixl 1 >>=
 
@@ -163,8 +163,8 @@ infixl 1 >>=
   -> (a -> k b)
   -> a
   -> k c
-(<=<) =
-  error "todo: Course.Monad#(<=<)"
+(<=<) bkc akb =
+  (bkc =<<) . akb
 
 infixr 1 <=<
 
